68eac38ad39da18c966ffaa538f6df43926b2aa0
Victor Keler <victor@keler.com>
2026-01-31 23:51:39 +0800

    решение проблемы ингреса смена NLB  на ELB,

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,9 @@ output "cluster_name" {
   description = "Имя EKS кластера"
   value       = var.cluster_name
 }
+
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs для NLB"
+  value       = [aws_subnet.public_zone_a.id, aws_subnet.public_zone_b.id]
+}
